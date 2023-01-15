a73bc3f991fb39a9b4a1b115e4e60c3b4fec9091
jesusocampo1105 <jesusocampo1105@gmail.com>
2023-01-15 16:20:39 -0500

    Add section Sobre mi

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+//import styles
+import "../styles/sobremi.css";
+//import images
+import image from "../img/ImageSobreMi.png";
+import whatsapp from "../img/whatsapp.png";
 
 const SobreMi = () => {
   return (
-    <div>SobreMi</div>
-  )
-}
+    <>
+      <article className="inicio">
+        <section className="inicio-2">
+          <img src={image} />
+        </section>
 
-export default SobreMi
+        <section className="inicio-1">
+          <h1>
+            Profesional en constante aprendizaje con conocimientos en desarrollo
+            web más específicamente en
+          </h1>
+          <h1>HTML, CSS, JavaScript y librerías como ReactJS.</h1>
+          <h1>
+            Habilidades de trabajo en equipo, liderazgo, comunicación asertiva,
+            buenas relaciones personales, alta adaptación al cambio.
+          </h1>
+        </section>
+
+        <a href="https://api.whatsapp.com/send?phone=573232014153&text=Hola%2C%20Vi%20tu%20CV%20y%20portafolio%20online...">
+          <img className="whatsapp" src={whatsapp} />
+        </a>
+      </article>
+    </>
+  );
+};
+
+export default SobreMi;
